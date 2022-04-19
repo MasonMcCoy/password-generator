cdf0a0859da985065044be31f27caedb60a0fa5f
Mason McCoy <mmasonmccoy@gmail.com>
2022-04-18 20:54:59 -0500

    Button click now generates a validated prompt and confirms for each possible password criteria.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function validLgth() {
+  var input = parseInt(prompt('Length?'));
+
+  if (isNaN(input)) {
+    return validLgth();
+  } else {
+    if (input < 8 || input > 129) {
+      return validLgth();
+    } else {
+      return input;
+    }
+  }
+}
+
 function generatePassword() {
-  return 'This works'
+  var lgth = validLgth();
+  var lowLet = confirm('Lowercase letters?');
+  var upLet = confirm('Uppercase letters?');
+  var nums = confirm('Numeric characters?');
+  var  spec = confirm('Special characters?');
+
+  console.log(lgth);
+  console.log(lowLet);
+  console.log(upLet);
+  console.log(nums);
+  console.log(spec);
 }
 
 // Write password to the #password input
